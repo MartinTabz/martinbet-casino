3245306010272df801ae084149a3b404cf7884d9
Martin Šíl <mail@martinsil.cz>
2025-03-23 21:23:17 +0100

    Making mines responsive

--- components/mines/MineBox.tsx
+++ components/mines/MineBox.tsx
@@ -37,19 +37,19 @@ export default function MineBox({
 				mine.revealed && mine.bomb && "border-red-900"
 			} ${
 				mine.revealed && !mine.bomb && "border-sky-800 bg-sky-950"
-			} w-full border-2 aspect-square rounded-sm relative flex items-center justify-center z-20`}
+			} w-full border-2 aspect-square overflow-hidden sm:overflow-visible rounded-sm relative flex items-center justify-center z-20`}
 		>
 			{mine.revealed && (
 				<>
 					{mine.bomb ? (
-						<div>
+						<div className="scale-75 sm:scale-100">
 							<Bomb />
 						</div>
 					) : (
-						<div>
+						<div className="scale-75 sm:scale-none">
 							<Diamond />
 							{mine.multiplier && (
-								<div className="absolute left-1/2 -translate-x-1/2 bg-sky-600 border-sky-800 border-2 rounded-md font-bold px-3 text-sm bottom-[-5px]">
+								<div className="absolute left-1/2 -translate-x-1/2 bg-sky-600 border-sky-800 border-2 rounded-md font-bold px-3 text-sm bottom-[-15px] sm:bottom-[-5px]">
 									{mine.multiplier}
 								</div>
 							)}
